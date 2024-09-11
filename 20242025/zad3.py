494247cec2f0061c660f6cb8345768f295b33197
HAJTI <deadend6464@gmail.com>
2024-09-11 09:44:47 +0200

    sad day

--- 20242025/zad3.py
+++ 20242025/zad3.py
@@ -1,12 +1,20 @@
-def sito_eratostenesa(n):
+def table(n):
     liczby = [True] * (n+1)
     liczby[0] = liczby[1] = False
-
     for i in range(2, int(n**0.5) + 1):
         if liczby[i]:
             for j in range(i*i, n+1, i):
                 liczby[j] = False
+    return liczby
 
+def sito_eratost(n):
+    liczby = table(n)
     return [x for x in range(2, n+1) if liczby[x]]
 
-print(sito_eratostenesa(100))
+def print(n):
+    primes = sito_eratost(n)
+    print(math.format(n))
+    print(format(n))
+    print(*primes, sep=", ")
+
+print(100)
