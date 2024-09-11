16fe49dd84f179a813c616283f6f50b556213680
HAJTI <deadend6464@gmail.com>
2024-09-11 09:32:05 +0200

    sito eratostenesa

---
+++ 20242025/zad3.py
@@ -0,0 +1,12 @@
+def sito_eratostenesa(n):
+    liczby = [True] * (n+1)
+    liczby[0] = liczby[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if liczby[i]:
+            for j in range(i*i, n+1, i):
+                liczby[j] = False
+
+    return [x for x in range(2, n+1) if liczby[x]]
+
+print(sito_eratostenesa(100))
