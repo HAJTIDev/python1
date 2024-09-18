ae42cc184b7da7cf87a2df2db3c1b7a431aeda92
HAJTI <deadend6464@gmail.com>
2024-09-18 09:44:53 +0200

    r

--- 20242025/CLASY.py
+++ 20242025/CLASY.py
@@ -1,24 +1,14 @@
 class Note:
-    __counter = 0
-
-    def __init__(self, title, content):
-        Note.__counter += 1
-        self.__id = Note.__counter
-        self._title = title
-        self._content = content
-
+    __counter=0
+    def __init__(self,title,content):
+        Note.__counter+=1
+        self.__id=Note.__counter
+        self._title=title
+        self._content=content
     def display(self):
-        print(f"Title: {self._title}\nContent: {self._content}")
-
+        print(f'title: {self._title}\nContent: {self._content}')
     def diagnose(self):
-        print(f"{self.__id};{self._title};{self._content}")
-
-
-# Test działania aplikacji
-note1 = Note("Title1", "Content1")
-note1.display()
-note1.diagnose()
-
-note2 = Note("Title2", "Content2")
-note2.display()
-note2.diagnose()
+        print(f'{self.__id};{self._title};{self._content}')
+notka=Note("meow","meow")
+notka.display()
+notka.diagnose()
